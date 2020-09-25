86e07c6349c60ac40f618f60b72ede9fe311a20e
Eco-Cult <70007776+Eco-Cult@users.noreply.github.com>
2020-09-25 11:30:06 +1000

    Update ec2_config1.2.js
    Configuration for a secondary setup

--- db/ec2_config1.2.js
+++ db/ec2_config1.2.js
@@ -1,1 +1,14 @@
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cbc',
+    password: 'ecocult',
+    port:'3306',
+    database:'CBC',
+    multipleStatements: true
+});
+
+//exported- to be used outside this file
+module.exports = connection;
